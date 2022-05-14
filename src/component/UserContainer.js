b2000d4393ec5b2b22c01005cee023998e5cfcf8
Binod Sharma <31683207+binod0233@users.noreply.github.com>
2022-05-14 09:09:40 +0545

    Update UserContainer.js

--- src/component/UserContainer.js
+++ src/component/UserContainer.js
@@ -37,7 +37,7 @@ const UserContainer = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       dispatch(fetchUserCategory());
-    }, 1000);
+    }, 100);
     return () => clearInterval(interval);
   }, [dispatch]);
 
